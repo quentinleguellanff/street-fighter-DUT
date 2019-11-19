96e6ed9a95c6d3eee1ca71a48805810d1a09ca06
Zenou Emmanuelle <emmanuelle.zenou@u-psud.fr>
2019-11-19 12:57:48 +0100

    Upload New File

---
+++ travail/menu/Broly.h
@@ -0,0 +1,35 @@
+#ifndef BROLY_H
+#define BROLY_H
+#include <SFML/Graphics.hpp>
+#include <iostream>
+#include <sstream>
+
+
+class Broly : public sf::Sprite
+{
+    private:
+        int _orientation;
+        sf::Texture _Texture;
+        int _scale;
+        int _cptanimstatic;
+        int _cptanimavancer;
+        int _cptanimjump;
+        int _cptAnimCoupPoing;
+        bool _ok;
+
+
+
+
+    public:
+        Broly(int);
+        void debout(sf::Clock& clock);
+        void avancer(sf::Clock& clock);
+        void reculer(sf::Clock& clock);
+        void sauter(sf::Clock& clock);
+        bool coupDePoing(sf::Clock& clock);
+        int getorientation();
+        virtual ~Broly();
+
+};
+
+#endif // BROLY_H
