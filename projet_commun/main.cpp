1392cfbc8b766f777ae15b50b2199800cc056cc7
Zenou Emmanuelle <emmanuelle.zenou@u-psud.fr>
2019-12-05 21:56:11 +0100

    Replace main.cpp

--- projet_commun/main.cpp
+++ projet_commun/main.cpp
@@ -4,7 +4,6 @@
 #include <SFML/Graphics.hpp>
 #include <SFML/Window.hpp>
 #include <SFML/System.hpp>
-#include <SFML/Audio.hpp>
 
 using namespace std;
 
@@ -12,11 +11,11 @@ int main()
 {
 	/* Gestion de la fenetre */
 	sf::RenderWindow window;
-	window.create(sf::VideoMode(1920,1080),"la Bagarre");
 	//window.create(sf::VideoMode(1920,1080),"la Bagarre",sf::Style::Fullscreen);
+	window.create(sf::VideoMode(1920,1080),"la Bagarre");
 	window.setVerticalSyncEnabled(true);
 	window.setFramerateLimit(60);
-	//window.setMouseCursorVisible(0);
+	window.setMouseCursorVisible(0);
 
 	/* Création de la scene */
 	Scene fond(window,1);
@@ -27,7 +26,9 @@ int main()
 	sf::Clock clockAnim_P2;
 	sf::Clock clockAttente_P2;
 
-	/* Création des deux champions */
+	/* Variable de création des deux champions */
+	//bool championsCrees=false;
+	//int selecChamp_P1=-1,selecChamp_P2=-1;
 	Jotaro champion_P1(-1,fond);
 	Dhalsim champion_P2(1,fond);
 
@@ -40,20 +41,24 @@ int main()
 	Player joueur2(2);
 
 	/* Création des variables pour les actions à effectuer */
-	bool apparitionsFinies=false,actionFini_P1=true,actionFini_P2=true;
-	bool peutmonter = true, peutdescendre = true;
-	int deplacementX_P1, deplacementY_P1, action_P1;
-	int deplacementX_P2, deplacementY_P2, action_P2;
+	bool apparitionsFinies_P1=false,apparitionsFinies_P2=false,actionFini_P1=true,actionFini_P2=true,etaitAccroupi_P1=false,etaitAccroupi_P2=false;
+	int deplacementX_P1, deplacementY_P1, action_P1, derniereAction_P1=-1;
+	int deplacementX_P2, deplacementY_P2, action_P2, derniereAction_P2=-1;
 
-    /* menu */
-	int go=0;
+	/* Déclarations pour le menu */
+	int selecEcran=0;
 
-	Menu menu(window.getSize().x, window.getSize().y);
+
+    Menu menu(window.getSize().x, window.getSize().y);
+
+    /* Déclaration menu selection */
+    MenuSelection menuSel(window);
+    sf::Event eventS;
 
 	/* Ouverture de la fenetre */
 	while(window.isOpen())
 	{
-
+		window.clear();
 		sf::Event event;
 
 		sf::Time elapsedAttente_P1 = clockAttente_P1.getElapsedTime();
@@ -62,188 +67,252 @@ int main()
     	sf::Time elapsedAttente_P2 = clockAttente_P2.getElapsedTime();
     	int timeAttente_P2 = elapsedAttente_P2.asMilliseconds();
 
-        if (go==0){
 
+    	if (selecEcran==0)
+    	{
             while (window.pollEvent(event))
-            {
-                switch ( event.type ){
+                menu.bouger(selecEcran,event, window);
 
-                case sf::Event::Closed:
-                        window.close( );
-                        break;
-                case sf::Event::KeyReleased:
-                    switch (event.key.code){
-                    case sf::Keyboard::Z:
-                      peutmonter = true;
-                      break;
-                    case sf::Keyboard::S:
-                        peutdescendre=true;
-                        break;}
-                }
-            }
-                if(sf::Keyboard::isKeyPressed(sf::Keyboard::Z)){
-                    if(peutmonter){
-                        menu.moveUp();
-                        peutmonter = false;
-                    }
-                }
-                if(sf::Keyboard::isKeyPressed(sf::Keyboard::S)){
-                    if(peutdescendre){
-                        menu.moveDown();
-                        peutdescendre = false;
-                    }
-                }
-
-            if ((sf::Keyboard::isKeyPressed(sf::Keyboard::Enter)) && (menu.getSelection()==0)){
-                go=1;
-                }
-            if ((sf::Keyboard::isKeyPressed(sf::Keyboard::Enter)) && (menu.getSelection()==2)){
-				window.close();
-                }
-
-        window.clear();
-        menu.draw(window);
-        window.display();
+	        menu.draw(window);
+	        window.display();
         }
 
-        else {
-		if(!apparitionsFinies)
-		{
-			apparitionsFinies=champion_P1.apparition(clockAnim_P1,effet_P1);
-			apparitionsFinies=champion_P2.apparition(clockAnim_P2,effet_P2);
-		}
-		else
-		{
-			/* Récuperation des actions à effectuer */
-			if(actionFini_P1)
+        else if(selecEcran==1)
+        {
+            while (window.pollEvent(eventS))
+	        {
+	            if (eventS.type == sf::Event::Closed)
+	                window.close();
+                    menuSel.bouger(eventS,window);
+                    selecEcran = menuSel.validationPerso(eventS);
+	        }
+
+	        /*if(selecEcran==2)
+	        {
+	        	switch(selecChamp_P1)
+				{
+				case 0:
+					Jotaro champion_P1(-1,fond);
+					break;
+				case 1:
+					Dhalsim champion_P1(-1,fond);
+					break;
+				}
+
+				switch(selecChamp_P2)
+				{
+				case 0:
+					Jotaro champion_P2(1,fond);
+					break;
+				case 1:
+					Dhalsim champion_P2(1,fond);
+					break;
+				}
+	        }*/
+
+            window.clear();
+            menuSel.draw(window);
+	        window.display();
+
+
+        }
+        else if(selecEcran==2)
+        {
+
+			if(!apparitionsFinies_P1 || !apparitionsFinies_P2)
 			{
-				champion_P1.reset();
-				joueur1.recuperationCommandesP1();
-				deplacementX_P1=joueur1.getPosHorizontale();deplacementY_P1=joueur1.getPosVerticale();action_P1=joueur1.getAction();
-			}else if(actionFini_P1==false && timeAttente_P1<150)
-			{
-				joueur1.recuperationAttaquesP1();
-				action_P1=joueur1.getAction();
+				if(!apparitionsFinies_P1)
+					apparitionsFinies_P1=champion_P1.apparition(clockAnim_P1,effet_P1);
+				if(!apparitionsFinies_P2)
+					apparitionsFinies_P2=champion_P2.apparition(clockAnim_P2,effet_P2);
 			}
-			if(actionFini_P2)
+			else
 			{
-				champion_P2.reset();
-				joueur2.recuperationCommandesP2();
-				deplacementX_P2=joueur2.getPosHorizontale();deplacementY_P2=joueur2.getPosVerticale();action_P2=joueur2.getAction();
-			}else if(actionFini_P2==false && timeAttente_P2<150)
-			{
-				joueur2.recuperationAttaquesP2();
-				action_P2=joueur2.getAction();
+				/* Récuperation des actions à effectuer */
+				if(actionFini_P1)
+				{
+					champion_P1.reset();
+					joueur1.recuperationCommandesP1();
+					deplacementX_P1=joueur1.getPosHorizontale();deplacementY_P1=joueur1.getPosVerticale();action_P1=joueur1.getAction();
+				}else if(actionFini_P1==false && timeAttente_P1<150)
+				{
+					joueur1.recuperationAttaquesP1();
+					action_P1=joueur1.getAction();
+				}
+				if(actionFini_P2)
+				{
+					champion_P2.reset();
+					joueur2.recuperationCommandesP2();
+					deplacementX_P2=joueur2.getPosHorizontale();deplacementY_P2=joueur2.getPosVerticale();action_P2=joueur2.getAction();
+				}else if(actionFini_P2==false && timeAttente_P2<150)
+				{
+					joueur2.recuperationAttaquesP2();
+					action_P2=joueur2.getAction();
+				}
+
+				/* Lancement des animations Player 1*/
+
+				if(action_P1!=derniereAction_P1 && derniereAction_P1==0 && deplacementY_P1==0)
+					actionFini_P1=champion_P1.finGarde(clockAnim_P1);
+				else if(etaitAccroupi_P1 && deplacementY_P1!=-1)
+					actionFini_P1=champion_P1.seLever(clockAnim_P1);
+				else if(deplacementX_P1==1 && deplacementY_P1==1)
+				{
+					if(champion_P1.getOrientation()==-1)
+						actionFini_P1=champion_P1.sauterAvant(clockAnim_P1,champion_P2);
+					else
+						actionFini_P1=champion_P1.sauterArriere(clockAnim_P1);
+				}
+				else if(deplacementX_P1==-1 && deplacementY_P1==1)
+				{
+					if(champion_P1.getOrientation()==-1)
+						actionFini_P1=champion_P1.sauterArriere(clockAnim_P1);
+					else
+						actionFini_P1=champion_P1.sauterAvant(clockAnim_P1,champion_P2);
+				}
+				else if(deplacementY_P1==1 && action_P1==2)
+					actionFini_P1=champion_P1.sautKick(clockAnim_P1);
+
+				else if(deplacementY_P1==1 && action_P1==1)
+					actionFini_P1=champion_P1.sautPunch(clockAnim_P1);
+
+				else if(deplacementX_P1==1)
+				{
+					if(champion_P1.getOrientation()==-1)
+						champion_P1.avancer(clockAnim_P1,champion_P2);
+					else
+						champion_P1.reculer(clockAnim_P1);
+				}
+				else if(deplacementX_P1==-1)
+				{
+					if(champion_P1.getOrientation()==-1)
+						champion_P1.reculer(clockAnim_P1);
+					else
+						champion_P1.avancer(clockAnim_P1,champion_P2);
+				}
+				else if(deplacementY_P1==1)
+					actionFini_P1=champion_P1.sauter(clockAnim_P1,clockAttente_P1);
+
+				else if(deplacementY_P1==-1)
+					champion_P1.accroupi(clockAnim_P1,action_P1==0);
+
+				else if(action_P1==0)
+					champion_P1.garde(clockAnim_P1);
+
+				else if(action_P1==1)
+					actionFini_P1=champion_P1.punch(clockAnim_P1);
+
+				else if(action_P1==2)
+					actionFini_P1=champion_P1.kick(clockAnim_P1);
+
+				else if(action_P1==3)
+					actionFini_P1=champion_P1.SP(clockAnim_P1,effet_P1);
+
+				else
+					champion_P1.statique(clockAnim_P1,champion_P2);
+
+				if(deplacementY_P1!=-1)
+					champion_P1.resetAccroupi();
+
+				if( !(action_P1!=derniereAction_P1 && derniereAction_P1==0) || actionFini_P1==true)
+					derniereAction_P1=action_P1;
+
+				if(actionFini_P1==true)
+					etaitAccroupi_P1=(deplacementY_P1==-1);
+
+				/* Lancement des animations Player 2*/
+
+				if(derniereAction_P2!=action_P2 && derniereAction_P2==0 && deplacementY_P2==0)
+					actionFini_P2=champion_P2.finGarde(clockAnim_P2);
+
+				else if(deplacementX_P2==1 && deplacementY_P2==1)
+				{
+					if(champion_P2.getOrientation()==1)
+						actionFini_P2=champion_P2.sauterAvant(clockAnim_P2,champion_P1);
+					else
+						actionFini_P2=champion_P2.sauterArriere(clockAnim_P2);
+				}
+				else if(deplacementX_P2==-1 && deplacementY_P2==1)
+				{
+					if(champion_P2.getOrientation()==1)
+						actionFini_P2=champion_P2.sauterArriere(clockAnim_P2);
+					else
+						actionFini_P2=champion_P2.sauterAvant(clockAnim_P2,champion_P1);
+				}
+				else if(deplacementY_P2==1 && action_P2==2)
+					actionFini_P2=champion_P2.sautKick(clockAnim_P2);
+
+				else if(deplacementY_P2==1 && action_P2==1)
+					actionFini_P2=champion_P2.sautPunch(clockAnim_P2);
+
+				else if(deplacementX_P2==1)
+				{
+					if(champion_P2.getOrientation()==1)
+						champion_P2.avancer(clockAnim_P2,champion_P1);
+					else
+						champion_P2.reculer(clockAnim_P2);
+				}
+				else if(deplacementX_P2==-1)
+				{
+					if(champion_P2.getOrientation()==1)
+						champion_P2.reculer(clockAnim_P2);
+					else
+						champion_P2.avancer(clockAnim_P2,champion_P1);
+				}
+				else if(deplacementY_P2==1)
+					actionFini_P2=champion_P2.sauter(clockAnim_P2,clockAttente_P2);
+
+				else if(deplacementY_P2==-1)
+					champion_P2.accroupi(clockAnim_P2,action_P2==0);
+
+				else if(action_P2==0)
+					champion_P2.garde(clockAnim_P2);
+
+				else if(action_P2==1)
+					actionFini_P2=champion_P2.prendCoup(clockAnim_P2);
+
+				else if(action_P2==2)
+					actionFini_P2=champion_P2.kick(clockAnim_P2);
+
+				else if(action_P2==3)
+					actionFini_P2=champion_P2.SP(clockAnim_P2,effet_P2);
+
+				else
+					champion_P2.statique(clockAnim_P2,champion_P1);
+
+				if(deplacementY_P2!=-1)
+					champion_P2.resetAccroupi();
+
+				if( !(action_P1!=derniereAction_P2 && derniereAction_P2==0) || actionFini_P2==true)
+					derniereAction_P2=action_P2;
 			}
-
-			/* Lancement des animations Player 1*/
-
-			if(deplacementX_P1==1 && deplacementY_P1==1)
-				actionFini_P1=champion_P1.sauterAvant(clockAnim_P1,champion_P2);
-
-			else if(deplacementX_P1==-1 && deplacementY_P1==1)
-				actionFini_P1=champion_P1.sauterArriere(clockAnim_P1);
-
-			else if(deplacementY_P1==1 && action_P1==2)
-				actionFini_P1=champion_P1.sautKick(clockAnim_P1);
-
-			else if(deplacementY_P1==1 && action_P1==1)
-				actionFini_P1=champion_P1.sautPunch(clockAnim_P1);
-
-			else if(deplacementX_P1==1)
-				champion_P1.avancer(clockAnim_P1,champion_P2);
-
-			else if(deplacementX_P1==-1)
-				champion_P1.reculer(clockAnim_P1);
-
-			else if(deplacementY_P1==1)
-				actionFini_P1=champion_P1.sauter(clockAnim_P1,clockAttente_P1);
-
-			else if(deplacementY_P1==-1)
-				champion_P1.accroupi(clockAnim_P1,action_P1==0);
-
-			else if(action_P1==0)
-				champion_P1.garde(clockAnim_P1);
-
-			else if(action_P1==1)
-				actionFini_P1=champion_P1.punch(clockAnim_P1);
-
-			else if(action_P1==2)
-				actionFini_P1=champion_P1.kick(clockAnim_P1);
-
-			else if(action_P1==3)
-				actionFini_P1=champion_P1.SP(clockAnim_P1,effet_P1);
-
-			else
-				champion_P1.statique(clockAnim_P1,champion_P2);
-
-			if(deplacementY_P1!=-1)
-				champion_P1.resetAccroupi();
-
-			/* Lancement des animations Player 2*/
-
-			if(deplacementX_P2==1 && deplacementY_P2==1)
-				actionFini_P2=champion_P2.sauterAvant(clockAnim_P2,champion_P1);
-
-			else if(deplacementX_P2==-1 && deplacementY_P2==1)
-				actionFini_P2=champion_P2.sauterArriere(clockAnim_P2);
-
-			else if(deplacementY_P2==1 && action_P2==2)
-				actionFini_P2=champion_P2.sautKick(clockAnim_P2);
-
-			else if(deplacementY_P2==1 && action_P2==1)
-				actionFini_P2=champion_P2.sautPunch(clockAnim_P2);
-
-			else if(deplacementX_P2==1)
-				champion_P2.avancer(clockAnim_P2,champion_P1);
-
-			else if(deplacementX_P2==-1)
-				champion_P2.reculer(clockAnim_P2);
-
-			else if(deplacementY_P2==1)
-				actionFini_P2=champion_P2.sauter(clockAnim_P2,clockAttente_P2);
-
-			else if(deplacementY_P2==-1)
-				champion_P2.accroupi(clockAnim_P2,action_P2==0);
-
-			else if(action_P2==0)
-				champion_P2.garde(clockAnim_P2);
-
-			else if(action_P2==1)
-				actionFini_P2=champion_P2.punch(clockAnim_P2);
-
-			else if(action_P2==2)
-				actionFini_P2=champion_P2.kick(clockAnim_P2);
-
-			else if(action_P2==3)
-				actionFini_P2=champion_P2.SP(clockAnim_P2,effet_P2);
-
-			else
-				champion_P2.statique(clockAnim_P2,champion_P1);
-
-			if(deplacementY_P2!=-1)
-				champion_P2.resetAccroupi();
 
 			/* Gestion de la fermeture de la fenetre */
-		}
+			while (window.pollEvent(event))
+	        {
+	            if (event.type == sf::Event::Closed)
+	                window.close();
+	        }
 
 
-		while (window.pollEvent(event))
-        {
-            if (event.type == sf::Event::Closed)
-                window.close();
-        }
+	        window.draw(fond.getSprite());
+	        window.draw(fond.getSol());
 
+	        window.draw(champion_P1);
+	        window.draw(effet_P1);
+	        window.draw(champion_P1.getHurtbox());
+	        window.draw(champion_P1.getHitbox());
 
-        window.draw(fond.getSprite());
-        window.draw(fond.getSol());
-        window.draw(champion_P1);
-        window.draw(champion_P2);
-        window.draw(effet_P1);
-        window.draw(effet_P2);
-        window.display();
+	        window.draw(champion_P2);
+	        window.draw(effet_P2);
+	        window.draw(champion_P2.getHurtbox());
+	        window.draw(champion_P2.getHitbox());
 
+	        window.display();
+	    }else if(selecEcran==-1)
+	    {
+	    	window.close();
+	    }
 	}
-	}
-
 }
+
