e3e4f101c0d20d6c74af0eaded0c72c4d013c4f2
Zenou Emmanuelle <emmanuelle.zenou@u-psud.fr>
2019-12-07 19:03:37 +0100

    Upload New File

---
+++ projetquimarcheaps/Player.h
@@ -0,0 +1,52 @@
+#ifndef PLAYER_H
+#define PLAYER_H
+
+#include <iostream>
+#include <string>
+
+#include <SFML/Graphics.hpp>
+#include <SFML/Window.hpp>
+#include <SFML/System.hpp>
+
+class Player
+{
+private:
+	int _numPlayer;
+	int _PV;
+	sf::RectangleShape _barrePV;
+
+	void _resetAttributs();
+	int _posHorizontale;
+	int _posVerticale;
+	int _action;
+	/* 	relation int-action
+	   -1 - rien
+	    0 - garde
+		1 - punch
+		2 - kick
+		3 - SP1
+	*/
+	sf::Event _eventTouche;
+	float joystick0_axisX;
+	float joystick0_axisY;
+	float joystick1_axisX;
+	float joystick1_axisY;
+
+public:
+	Player(){};
+	Player(int,sf::RenderWindow&);
+	void recuperationCommandesP1();
+	void recuperationAttaquesP1();
+	void recuperationCommandesP2();
+	void recuperationAttaquesP2();
+	void gestionDesCommandes(bool avancer, bool reculer, bool accroupi, bool saut, bool sautAvant, bool sautArriere,bool garde, bool punch, bool kick, bool SP1);
+
+	int getPosHorizontale();
+	int getPosVerticale();
+	int getAction();
+	int getPV();
+	void setDegats(int);
+	sf::RectangleShape getBarrePV();
+};
+
+#endif
